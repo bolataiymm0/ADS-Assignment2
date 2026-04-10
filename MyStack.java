bac17cc4a3d7636bfa18ee0dd9645da3d5749451
bolataiymm0 <bolataiymm@gmail.com>
2026-04-10 16:02:57 +0500

    Delete MyStack.java

--- MyStack.java
+++
@@ -1,7 +0,0 @@
-public class MyStack<T extends Comparable<T>> {
-    private MyArrayList<T> list = new MyArrayList<>();
-    public void push(T item) { list.add(item); }
-    public T pop() { T item = list.getLast(); list.removeLast(); return item; }
-    public T peek() { return list.getLast(); }
-    public boolean isEmpty() { return list.size() == 0; }
-}
